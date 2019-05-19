2227183ea99dd4fa09a05f1623773b98b1072180
Bugra Alkin <alknbugra@gmail.com>
2019-05-19 22:45:33 +0300

    Add files via upload

---
+++ ders4.c
@@ -0,0 +1,16 @@
+//ders 4 dosya çaðýrma
+#include<stdio.h>
+
+#define BOY 100
+#define SAYI 3.14
+#define HARF 'A'
+#define harf_dizim "ABC"
+#define backslash_char '\?'
+
+void main(){
+	printf("value of height : %d \n",BOY);
+	printf("value of number : %f \n",SAYI);
+	printf("value of letter : %c \n",HARF);
+	printf("value of harf dizilimi : %s \n",harf_dizim);
+	printf("value of blackslash_char : %c \n",backslash_char);
+}
